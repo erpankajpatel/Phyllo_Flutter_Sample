987b4032ae4a6457b3a0b0223ef486e23718db27
Pankaj Patel <patelpankaj.1010@gmail.com>
2022-04-25 10:47:01 +0530

    - Flutter Phyllo SDk uses sample app

---
+++ lib/constants/configs.dart
@@ -0,0 +1,11 @@
+import 'package:phyllo_connect/phyllo_connect.dart';
+
+class Configs {
+  Configs._();
+
+  static const String clientId = '';
+
+  static const String clientSecret = '';
+
+  static const PhylloEnvironment environment = PhylloEnvironment.development;
+}
---
+++ lib/phyllo_provider.dart
@@ -0,0 +1,126 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_sample_app/client/phyllo_repository.dart';
+import 'package:flutter_sample_app/constants/configs.dart';
+import 'package:phyllo_connect/phyllo_connect.dart';
+
+abstract class DefaultChangeNotifier extends ChangeNotifier {
+  bool _loading = false;
+
+  void setLoading(bool state) {
+    _loading = state;
+    notify();
+  }
+
+  void notify() {
+    notifyListeners();
+  }
+
+  bool get isLoading => _loading;
+}
+
+class PhylloProvider extends DefaultChangeNotifier {
+  bool _isExistingUser = false;
+  static const String clientDisplayName = 'Phyllo Test';
+  static String? _token;
+  static String? _userId;
+
+  final PhylloConnect _phylloConnect = PhylloConnect.instance;
+  final PhylloRepository _phylloRepository = PhylloRepository.instance;
+  //
+  void launchSdk(String workPlatformId) async {
+    try {
+      setLoading(true);
+
+      String? env = await getPhylloEnvironmentUrl(Configs.environment);
+
+      if (env != null) {
+        if (_isExistingUser) {
+          _token = await getSdkToken(env, userId!);
+          if (_token != null) {
+            _launchSdk(workPlatformId);
+          }
+        } else {
+          // Get UserId
+          _userId = await getUserId(env);
+          if (_userId != null) {
+            // Get Token
+            _token = await getSdkToken(env, _userId!);
+            if (_token != null) {
+              _launchSdk(workPlatformId);
+            }
+          }
+        }
+      }
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  void _launchSdk(String workPlatformId) {
+    PhylloConfig config = PhylloConfig(
+      clientDisplayName: clientDisplayName,
+      environment: Configs.environment,
+      userId: _userId!,
+      token: _token!,
+      workPlatformId: workPlatformId,
+    );
+    _phylloConnect.initialize(config);
+    _phylloConnect.open();
+
+    _phylloConnect.onConnectCallback(
+        onAccountConnected: (account_id, work_platform_id, user_id) {
+      log('onAccountConnected: $account_id, $work_platform_id, $user_id');
+    }, onAccountDisconnected: (account_id, work_platform_id, user_id) {
+      log('onAccountDisconnected: $account_id, $work_platform_id, $user_id');
+    }, onTokenExpired: (user_id) {
+      log('onTokenExpired: $user_id');
+    }, onExit: (reason, user_id) {
+      log('onExit: $reason, $user_id');
+    });
+  }
+
+  Future<String?> getPhylloEnvironmentUrl(PhylloEnvironment environment) async {
+    try {
+      String? env = await _phylloConnect.getPhylloEnvironmentUrl(environment);
+      return env!;
+    } catch (e) {
+      debugPrint(e.toString());
+      return null;
+    }
+  }
+
+  Future<String?> getUserId(String envUrl) async {
+    try {
+      setLoading(true);
+      String? userId = await _phylloRepository.getUserId(envUrl);
+      return userId;
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  Future<String?> getSdkToken(String envUrl, String userId) async {
+    try {
+      setLoading(true);
+      String? token =
+          await _phylloRepository.getSdkToken(envUrl, userId: userId);
+      return token;
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  void isExistingUserStatusChanged(bool? value) {
+    _isExistingUser = value!;
+    notify();
+  }
+
+  bool get isExistingUser => _isExistingUser;
+
+  String? get userId => _userId;
+}
